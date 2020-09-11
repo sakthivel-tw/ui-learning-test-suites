35258c0628ab8f23d08a66ae286525fccda0c472
Sakthivel Viswanathan <sakthivel.v@thoughtworks.com>
2020-09-11 17:21:25 +0530

    Add tests for datatypes

---
+++ javascript/01_datatypes/datatypes.test.js
@@ -0,0 +1,40 @@
+describe("primitive and structural datatypes", () => {
+  test("typeof", () => {
+    var foo = "String";
+    expect(typeof foo).toBe();
+
+    foo = false;
+    expect(typeof foo).toBe();
+
+    foo = 123.54;
+    expect(typeof foo).toBe();
+
+    foo = -31;
+    expect(typeof foo).toBe();
+
+    foo = 1;
+    expect(typeof foo).toBe();
+
+    foo = {};
+    expect(typeof foo).toBe();
+
+    foo = function () {};
+    expect(typeof foo).toBe();
+
+    foo = () => {}; // Arrow function
+    expect(typeof foo).toBe();
+
+    var bar = Symbol("Test");
+    var boo = Symbol("Test");
+    expect(bar === boo).toBe();
+  });
+
+  test("undefined & null", () => {
+    var foo;
+    var bar = null;
+
+    expect(typeof foo).toBe();
+    expect(typeof bar).toBe();
+    expect(foo !== bar).toBe();
+  });
+});
