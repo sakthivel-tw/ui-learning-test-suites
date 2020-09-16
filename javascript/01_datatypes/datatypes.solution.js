7f8212c8e54eb10681eff58304998f919ce163c8
Sakthivel Viswanathan <sakthivel.v@thoughtworks.com>
2020-09-16 16:42:48 +0530

    Added samples for use strict, let vs const

---
+++ javascript/01_datatypes/datatypes.solution.js
@@ -0,0 +1,40 @@
+describe("primitive and structural datatypes", () => {
+  test("typeof", () => {
+    var foo = "String";
+    expect(typeof foo).toBe("string");
+
+    foo = false;
+    expect(typeof foo).toBe("boolean");
+
+    foo = 123.54;
+    expect(typeof foo).toBe("number");
+
+    foo = -31;
+    expect(typeof foo).toBe("number");
+
+    foo = 1;
+    expect(typeof foo).toBe("number");
+
+    foo = {};
+    expect(typeof foo).toBe("object");
+
+    foo = function () {};
+    expect(typeof foo).toBe("function");
+
+    foo = () => {}; // Arrow function
+    expect(typeof foo).toBe("function");
+
+    var bar = Symbol("Test");
+    var boo = Symbol("Test");
+    expect(bar === boo).toBe(false);
+  });
+
+  test("undefined & null", () => {
+    var foo;
+    var bar = null;
+
+    expect(typeof foo).toBe("undefined");
+    expect(typeof bar).toBe("object");
+    expect(foo !== bar).toBe(true);
+  });
+});
---
+++ javascript/02_use_strict_let_vs_const/useStrictLetAndConst.test.js
@@ -0,0 +1,116 @@
+describe("use strict, let & const", () => {
+  test("throw errors when using use strict", () => {
+    // ======================================================= //
+
+    expect(() => {
+      function foo() {
+        x = 1;
+        return "ok";
+      }
+      foo();
+    }).toThrow(new ReferenceError("x is not defined"));
+
+    // ======================================================= //
+
+    function foo() {
+      y = 4;
+    }
+    foo();
+    expect(y).toBe(4);
+
+    // ======================================================= //
+
+    var user = {
+      name: "foo",
+      profession: "dev",
+    };
+    delete user.name;
+    expect(user).toEqual();
+  });
+
+  test("var, let & const", () => {
+    // ======================================================= //
+
+    function foo() {
+      var x = 1;
+      return x;
+    }
+    expect(foo()).toEqual();
+
+    // ======================================================= //
+
+    function bar() {
+      if (true) {
+        var y = 3;
+      }
+      return y;
+    }
+    expect(bar()).toEqual();
+
+    // ======================================================= //
+    // Make the below function throw an exception
+
+    expect(() => {
+      function baz() {
+        if (true) {
+          let z = 1;
+        }
+      }
+      baz();
+    }).toThrow();
+
+    // ======================================================= //
+
+    function bay() {
+      let x = "outer";
+      if (true) {
+        let x = "inner";
+      }
+      return x;
+    }
+    expect(bay()).toEqual();
+
+    // ======================================================= //
+
+    function baf() {
+      let x = "outer";
+      {
+        let x = "inner";
+      }
+      return x;
+    }
+    expect(baf()).toEqual();
+
+    // ======================================================= //
+    // Make the below function throw an exception
+
+    expect(() => {
+      function bae() {
+        const x = [1, 2];
+
+        return x;
+      }
+      bae();
+    }).toThrow(new TypeError("Assignment to constant variable."));
+
+    // ======================================================= //
+
+    function bax() {
+      const arr = [1, 2, 5];
+      arr.push(6);
+      return arr;
+    }
+    expect(bax()).toEqual();
+
+    // ======================================================= //
+
+    function bav() {
+      const obj = { name: "foo", age: 40 };
+      obj.profession = "dev";
+      obj["age"] = 51;
+      delete obj.name;
+      return obj;
+    }
+    expect(bav()).toEqual();
+  });
+});
